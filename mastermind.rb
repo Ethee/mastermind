e7a4b6f4bba574d10baf4931e2c4a59814472ccf
Jacob Mattern <jakemmattern@gmail.com>
2021-04-16 11:13:36 -0400

    Define game structure

---
+++ mastermind.rb
@@ -0,0 +1,24 @@
+#frozen_string_literal: true
+
+class State
+  def initialize
+  end
+end
+
+class Display
+  def rules
+  end
+  def boardState
+  end
+end
+
+class Turns
+end
+
+class AI
+end
+
+def game_start
+  Display.new.rules
+  
+end
